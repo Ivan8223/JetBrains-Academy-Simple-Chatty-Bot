5fc57ea1e22635885006195dfee3016d2da2034e
Ivan <ivan.mjasoyedov@gmail.com>
2021-01-06 18:29:02 +0200

    first commit

---
+++ Problems/Minimum and maximum/main.py
@@ -0,0 +1,7 @@
+num1 = int(input())
+num2 = int(input())
+
+if num1 >= num2:
+    print(f'{num1}\n{num2}')
+else:
+    print(f'{num2}\n{num1}')
---
+++ Problems/Spellchecker/main.py
@@ -0,0 +1,8 @@
+dictionary = ["aa", "abab", "aac", "ba", "bac", "baba", "cac", "caac"]
+
+world = input()
+
+if world in dictionary:
+    print('Correct')
+else:
+    print('Incorrect')
